20fd8aabbf123153487652d2e05e06886f88e1bc
jayg791 <jamesgallagher791@gmail.com>
2017-05-20 12:16:09 +0100

    R Calculator complete

--- Calculator Calls.R
+++ Calculator Calls.R
@@ -8,3 +8,35 @@ Addition(5,10)
 
 Subtraction(24, 14)
 
+# Multiply call
+
+Multiply(11, 67)
+
+# Divide call
+
+Divide (5, 0)
+Divide (22, 11)
+
+# Remainder call
+
+Remainder (17, 9)
+
+# Random Vector call
+
+Random_Vector()
+
+# Exponent Function
+
+Expo(5, 2)
+
+# Factorial Function
+
+Factorial(9)
+
+# Square Root call
+
+Square_Root(100)
+
+# Sum of Squares 
+
+Sum_of_Squares(9,9)
